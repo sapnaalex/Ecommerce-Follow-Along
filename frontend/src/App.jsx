ab6dadeb34b0c133d5294f811758ff3da999f166
unknown <sapna.alex03@gmail.com>
2025-03-05 12:05:21 +0530

    Follow Along Milestone 21

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LoginPage, SignupPage, Home, CreateProduct, MyProducts, Cart, ProductDetails, Profile } from "./Routes.jsx";
 import "./App.css";
+import CreateAddress from './pages/createAddress.jsx';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="/product/:id" element={<ProductDetails />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/create-address" element={<CreateAddress/>}/>
       </Routes>
     </BrowserRouter>
   );
